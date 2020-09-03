ab6d2dbf9509648981ddcce8d1030d1504c480d2
William Estoque <william.estoque@gmail.com>
2020-09-03 18:08:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# alb_support
+
+Extracted from `aws-samples` to make it easier to add ALB support to an OpsWorks layer._
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer 'William Estoque'
+name 'alb_support'
+maintainer_email 'william.estoque@gmail.com'
+license 'Apache-2.0'
+description 'Add ALB Support to an OpsWorks Layer.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '1.0.0'
