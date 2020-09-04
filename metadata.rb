77f6034041e4075e7a0a45b48e230ff805b12d2b
William Estoque <william.estoque@gmail.com>
2020-09-03 22:07:58 -0400

    Use version 2 for aws-sdk-core

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'william.estoque@gmail.com'
 license 'Apache-2.0'
 description 'Add ALB Support to an OpsWorks Layer.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.1.0'
